24c3b96448372e4d693fd766e12fe669f4e56db3
Hiếu <giaochu.nd@gmail.com>
2022-11-01 08:15:38 +0700

    Update install.sh

--- 20.04/install.sh
+++ 20.04/install.sh
@@ -4,7 +4,7 @@ set -e
 CURRENT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 source ${CURRENT_DIR}/../common/common.sh
 
-[ $(id -u) != "0" ] && { ansi -n --bold --bg-red "请用 root 账户执行本脚本"; exit 1; }
+[ $(id -u) != "0" ] && { ansi -n --bold --bg-red "vui long dung tai khoan root"; exit 1; }
 
 MYSQL_ROOT_PASSWORD=`random_string`
 
@@ -26,23 +26,6 @@ function init_alias {
     alias sudowww > /dev/null 2>&1 || {
         echo "alias sudowww='sudo -H -u ${WWW_USER} sh -c'" >> ~/.bash_aliases
     }
-}
-
-function init_repositories {
-    add-apt-repository -y ppa:ondrej/php
-    add-apt-repository -y ppa:nginx/stable
-    grep -rl ppa.launchpad.net /etc/apt/sources.list.d/ | xargs sed -i 's/http:\/\/ppa.launchpad.net/https:\/\/launchpad.proxy.ustclug.org/g'
-
-    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list
-
-    # https://mirrors.tuna.tsinghua.edu.cn/  2021-02-05移除 nodesource 镜像
-
-    curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
-    echo 'deb https://deb.nodesource.com/node_10.x focal main' > /etc/apt/sources.list.d/nodesource.list
-    echo 'deb-src https://deb.nodesource.com/node_10.x focal main' >> /etc/apt/sources.list.d/nodesource.list
-
-    apt-get update
 }
 
 function install_basic_softwares {
@@ -73,14 +56,13 @@ function install_composer {
     sudo -H -u ${WWW_USER} sh -c  'cd ~ && composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/'
 }
 
-call_function init_system "正在初始化系统" ${LOG_PATH}
-call_function init_repositories "正在初始化软件源" ${LOG_PATH}
-call_function install_basic_softwares "正在安装基础软件" ${LOG_PATH}
-call_function install_php "正在安装 PHP" ${LOG_PATH}
-call_function install_others "正在安装 Mysql / Nginx / Redis / Memcached / Beanstalkd / Sqlite3" ${LOG_PATH}
-call_function install_node_yarn "正在安装 Nodejs / Yarn" ${LOG_PATH}
-call_function install_composer "正在安装 Composer" ${LOG_PATH}
+call_function init_system "khoi tao" ${LOG_PATH}
+call_function install_basic_softwares "cai dat co ban" ${LOG_PATH}
+call_function install_php "cai dat PHP" ${LOG_PATH}
+call_function install_others "cai dat Mysql / Nginx / Redis / Memcached / Beanstalkd / Sqlite3" ${LOG_PATH}
+call_function install_node_yarn "cai dat Nodejs / Yarn" ${LOG_PATH}
+call_function install_composer "cai dat Composer" ${LOG_PATH}
 
-ansi --green --bold -n "安装完毕"
-ansi --green --bold "Mysql root 密码："; ansi -n --bold --bg-yellow --black ${MYSQL_ROOT_PASSWORD}
+ansi --green --bold -n "cai dat"
+ansi --green --bold "Mysql root pass："; ansi -n --bold --bg-yellow --black ${MYSQL_ROOT_PASSWORD}
 ansi --green --bold -n "请手动执行 source ~/.bash_aliases 使 alias 指令生效。"
